9209b3c03c22fed040a16b3a16d9432bfd2b80af
Aromakc <suwashkhatry38@gmail.com>
2020-08-04 09:12:14 +0545

    user add problem unsolved

--- createaccount.cpp
+++ createaccount.cpp
@@ -21,10 +21,10 @@ CreateAccount::~CreateAccount()
 }
 
 void CreateAccount::on_pushButton_create_clicked()
-{
+/*{
 
-        QString name,address,password,phone,username,gender;
-
+        QString name,address,password,username,gender;
+        qint64 phone;
         username=ui->line_username->text();
         password=ui->line_password->text();
         name=ui->line_name->text();
@@ -37,24 +37,71 @@ void CreateAccount::on_pushButton_create_clicked()
 
 
 
-        QSqlQuery qry;
-        //qry.prepare("INSERT into Users (username,password,Name,Gender,Address,Phone_Number) values ('"+username+"','"+password+"','"+name+"','"+gender+"','"+address+"',"+phone+")");
+        QSqlQuery query;
+        query.prepare("INSERT into Users (username,password,Name,Gender,Address,Phone_Number) values ('"+username+"','"+password+"','"+name+"','"+gender+"','"+address+"',"+phone+")");
 
-        qry.prepare("INSERT into Users (username,password,Name,Gender,Address,Phone_Number) VALUES(?, ?, ?, ?, ? ,?)");
+        /*query.prepare("INSERT into Users (username,password,Name,Gender,Address,Phone_Number) VALUES(?, ?, ?, ?, ? ,?)");
 
-        qry.addBindValue(username);
-        qry.addBindValue(password);
-        qry.addBindValue(name);
-        qry.addBindValue(gender);
-        qry.addBindValue(address);
-        qry.addBindValue(phone);
+        query.addBindValue(username);
+        query.addBindValue(password);
+        query.addBindValue(name);
+        query.addBindValue(gender);
+        query.addBindValue(address);
+        query.addBindValue(phone);
 
 
-        if(qry.exec()){
+        if(query.exec()){
              QMessageBox::information(this,tr("Save"),tr("Saved"));}
         else
         {
-            QMessageBox::critical(this,tr("Error."),qry.lastError().text());
+            QMessageBox::critical(this,tr("Error."),query.lastError().text());
         }
+}*/
+{
+    QString name,address,password,username,gender;
+    qint64 phone;
+    bool ok;
+    username=ui->line_username->text();
+    password=ui->line_password->text();
+    name=ui->line_name->text();
+    address=ui->line_address->text();
+    phone=ui->line_phone->text().toInt(&ok);
+    if(ui->radioButton_M->isChecked())
+    { gender='M';}
+    if(ui->radioButton_F->isChecked())
+    { gender='F';}
+
+    if(ok){
+
+        QSqlQuery query;
+       //query.prepare("INSERT into Users (username,password,Name,Gender,Address,Phone_Number) values ('"+username+"','"+password+"','"+name+"','"+gender+"','"+address+"','"+phone+"')");
+
+        query.prepare("INSERT into Users (username,password,Name,Gender,Address,Phone_Number) VALUES(:u.:p,:n,:g,:a,:c)");
+
+        query.bindValue(":u",username);
+        query.bindValue(":p",password);
+        query.bindValue(":n",name);
+        query.bindValue(":g",gender);
+        query.bindValue(":a",address);
+        query.bindValue(":c",phone);
+        qDebug()<< query.lastQuery();
+    if(query.exec()){
+        QMessageBox::information(this,tr("Save"),tr("Saved"));
+       /* ui->line_name->clear();
+        ui->line_username->clear();
+        ui->line_password->clear();
+        ui->line_address->clear();
+        ui->line_phone->clear();*/
+        }
+    else
+    {
+        QMessageBox::critical(this,tr("Error."),query.lastError().text());         //?????????
+    }
+    }
+     else
+    {
+        QMessageBox::information(this, "Invalid Input", "Enter Proper Number");
+    }
 }
+
 
